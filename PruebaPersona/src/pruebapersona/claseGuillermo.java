f48f0eb51e278e119064db7f2be1e134bbbf3dfe
Guille <ffetisovv96@gmail.com>
2022-02-02 19:30:38 +0100

    repositorio remoto 1

---
+++ PruebaPersona/src/pruebapersona/claseGuillermo.java
@@ -0,0 +1,11 @@
+package pruebapersona;
+
+public class claseGuillermo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo");
+
+	}
+
+}
