23078f06f7f3154b26da712c09b1ed8c3afeb607
Guille <ffetisovv96@gmail.com>
2022-02-02 19:50:38 +0100

    commit de amigo

---
+++ PruebaPersona/src/pruebapersona/claseAmigo.java
@@ -0,0 +1,11 @@
+package pruebapersona;
+
+public class claseAmigo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("soy tu amigo");
+
+	}
+
+}
